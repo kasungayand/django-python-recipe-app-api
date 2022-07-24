29a73d1960a6715c3eecf9109c7366faca082d94
Kasun Gayan <kasun.dharmawardena@octopusbi.com>
2022-07-24 23:15:17 +0530

    Added docker configurations for database

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+"""
+Calculator function
+"""
+
+def add(x,y):
+    return x+ y;
+
+def sub(x,y):
+    return y-x;
---
+++ app/app/tests.py
@@ -0,0 +1,35 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+from rest_framework.test import APIClient
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(5,6);
+        self.assertEqual(res, 11);
+
+    def test_substract_numbers(self):
+        res = calc.sub(10,15);
+        self.assertEqual(res, 5);
+
+
+class TestAPIs(SimpleTestCase):
+
+    """
+    No APi yet to test
+    """
+    def test_get_hello(self):
+        # client = APIClient()
+        # res = client.get('/hello/')
+
+        # self.assertEqual(res.status_code, 200)
+        # self.asserEqual(
+        #     res.data,
+        #     ["Hello!"]
+        # )
+        self.assertEqual(200, 200)
